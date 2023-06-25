c12fe3f4687312b4504d762fc4317ff1ef136c95
JevonAlSalgus <136735889+JevonAlSalgus@users.noreply.github.com>
2023-06-26 01:23:37 +0700

    Add files via upload

---
+++ grocerystore-main/index.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>TEKKOM DISTRIBUTOR WEBSITE</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"/>
+        <!-- font Awesome cdn link   -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+
+        <!-- custom css file link -->
+        <link rel="stylesheet" href="./style.css">
+
+
+
+    </head>
+    <body>
+        <!-- header section starts -->
+        <header class="header">
+            <a href="#" class="logo"> <i class="fas fa-shopping-basket"></i> TEKKOM DISTRIBUTOR </a>
+            <nav class="navbar">
+                <a href="#home">home</a>
+                <a href="#features">features</a>
+                <a href="#categories">product categories</a> 
+                <a href="login.php">Login</a> 
+            </nav>
+
+        </header>
+
+<section class="home" id="home">
+
+    <div class="content">
+        <h3>Good and <span>Best Quality</span> products for your minimarket</h3>
+        <p>Providing Quality Products and Services to All Minimarket</p>
+    </div>
+
+</section>
+
+    
+    <section class="features" id="features">
+
+        <h1 class="heading"> our <span>features</span> </h1>
+
+        <div class="box-container">
+
+            <div class="box">
+                <img src="image/feature-img-1.png" alt="">
+                <h3>fresh and best quality</h3>
+                <p>Pasti Terjamin!</p>
+            
+            </div>
+
+            <div class="box">
+                <img src="image/feature-img-2.png" alt="">
+                <h3>free delivery</h3>
+                <p>Pasti Gratis!</p>
+             
+            </div>
+
+            <div class="box">
+                <img src="image/feature-img-3.png" alt="">
+                <h3>easy payments</h3>
+                <p>Pasti Mudah!</p>
+            
+            </div>
+
+        </div>
+
+    </section>
+
+
+    <section class="categories" id="categories">
+
+        <h1 class="heading"> product <span>categories</span> </h1>
+
+        <div class="box-container">
+
+            <div class="box">
+                <img src="image/cat-1.png" alt="">
+                <h3>sayuran</h3>
+            </div>
+
+            <div class="box">
+                <img src="image/cat-2.png" alt="">
+                <h3>buah segar</h3>
+            </div>
+
+            <div class="box">
+                <img src="image/makanan.png" alt="">
+                <h3>Makanan</h3>
+            </div>
+
+            <div class="box">
+                <img src="image/minuman.png" alt="">
+                <h3>Minuman</h3>
+            </div>
+
+        </div>
+
+    </section>
+
+
+    <section class="footer">
+
+        <div class="box-container">
+
+            <div class="box">
+                <h3> TEKKOM DISTRIBUTOR <i class="fas fa-shopping-basket"></i> </h3>
+                <p>Providing Quality Products And Services To Minimarket Distributors</p>
+                <div class="share">
+                    <a href="#" class="fab fa-facebook-f"></a>
+                    <a href="#" class="fab fa-twitter"></a>
+                    <a href="#" class="fab fa-instagram"></a>
+                    <a href="#" class="fab fa-linkedin"></a>
+                </div>
+            </div>
+
+            <div class="box">
+                <h3>contact info</h3>
+                <a href="#" class="links"> <i class="fas fa-phone"></i> +6282384652041 </a>
+                <a href="#" class="links"> <i class="fas fa-phone"></i> +6282354667127 </a>
+                <a href="#" class="links"> <i class="fas fa-envelope"></i> tekkomdistributor@gmail </a>
+                <a href="#" class="links"> <i class="fas fa-map-marker-alt"></i> Padang, Indonesia </a>
+            </div>
+
+            <div class="box">
+                <h3>quick links</h3>
+                <a href="#" class="links"> <i class="fas fa-arrow-right"></i> home </a>
+                <a href="#" class="links"> <i class="fas fa-arrow-right"></i> features </a>
+                <a href="#" class="links"> <i class="fas fa-arrow-right"></i> product categories </a>
+
+            </div>
+
+        </div>
+
+        <div class="credit"> copyright <span>TEKKOM DISTRIBUTOR</span></div>
+
+    </section>
+
+        <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
+
+        <script src="./script.js" async defer></script>
+    </body>
+</html>
